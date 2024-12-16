ff367a4547896792d32e861a3bd45e7be081c7e7
Natalia Tomashenko <tomashenko.n@gmail.com>
2024-12-16 14:01:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ pip install -U pip || exit 1
 
 pip install -r requirements.txt || exit 1
 
-git clone https://github.com/speechbrain/speechbrain.git || exit 1
-
-cd speechbrain
-
-pip install -e . || exit 1
-
-pip install -r recipes/LJSpeech/TTS/extra_requirements.txt || exit 1
+if [ ! -d speechbrain ]; then
+    git clone https://github.com/speechbrain/speechbrain.git || exit 1
+    cd speechbrain
+    pip install -e . || exit 1
+    pip install -r recipes/LJSpeech/TTS/extra_requirements.txt || exit 1
+    cd ..
+fi
 
 wget -c https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 || exit 1
 
